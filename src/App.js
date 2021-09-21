2752ec51856a79b5f38702469556fa38fb638430
Aidan McKay <aidanmckay2000@gmail.com>
2021-09-21 12:43:07 -0500

    Add fetch call for nasa images and return component for images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import ImageCard from './ImageCard'
+
 
 function App() {
+  const [page, setPage] = useState(1)
+  const [nasaPictures, setNasaPictures] = useState([])
+  const [isLoading, setLoading] = useState(true);
+  const API_KEY = process.env.REACT_APP_API_KEY;
+
+  const loadMoreImages = () => {
+    setPage(page +1)
+  }
+
+  useEffect(() => {
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=5&thumbs=true`)
+    .then(res => res.json())
+    .then(data => {
+      setNasaPictures(data)
+      setLoading(false)
+    })
+    .catch(error => console.log(error))
+  }, [page])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <ImageCard props={nasaPictures} />
+    </>
+  )
 }
 
 export default App;
